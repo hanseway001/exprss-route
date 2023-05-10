623d90ab5f4a56b5349a72894d4e381be70c83ce
Hanseway001 <wayne.hansen9276@stu.mtec.edu>
2023-05-10 09:35:51 -0600

    first commit

---
+++ index.js
@@ -0,0 +1,30 @@
+const express = require('express')
+const app = express()
+const port = process.env.PORT || 3008
+app.use(express.urlencoded({extended:false}))
+app.use(express.json())
+
+app.get('/*pp*', (req,res) => {
+    res.send('good')
+})
+
+app.get('/users/:userId/color/:color/', (req,res) => {
+    let id = req.params.userId
+    console.log(req.params)
+    console.log(id)
+    res.send(`your user id is: ${id} ${req.params.color}`)
+})
+
+app.route('/route')
+.get((req, res) => {
+    res.send('from a get request')
+})
+.post((req, res) => {
+    let count = req.body.count
+    let color = req.body.color
+    res.send(`from a post request count is ${count} color is ${color}`)
+})
+
+app.listen(port, () => {
+    console.log(`server is up on port: ${port}`)
+})
